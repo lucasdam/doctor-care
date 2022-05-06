240e61b3c28fcd90f06743761efda0d865a9c66d
Lucas Damasceno <54273070+lucasdam@users.noreply.github.com>
2022-05-06 13:45:22 -0300

    Add desktop version and logic to mark current navigation session

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,32 @@ onScroll()
 function onScroll() {
     showNavOnScroll()
     showBackToTopButtonOnScroll()
+    activeteMenuAtCurrentSection(home)
+    activeteMenuAtCurrentSection(services)
+    activeteMenuAtCurrentSection(about)
+}
+
+function activeteMenuAtCurrentSection(section) {
+    const targetLine = scrollY + innerHeight / 2
+
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+    const sectionEndsAt = sectionTop + sectionHeight
+
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+        menuElement.classList.add('active')
+    }
 }
 
 function showNavOnScroll() {
